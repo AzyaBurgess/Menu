4d166b10d7a8609328473ea62c6b3d03470dafb2
AzyaBurgess <Azya.Burgess@cdpipelinedevshop.com>
2022-08-08 16:59:45 +0000

    first commit

---
+++ menu/src/App.js
@@ -0,0 +1,12 @@
+import Album from './components/Album';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <Album></Album>  
+    </div>
+  );
+}
+
+export default App;
